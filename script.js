dc1d5e2b72bfe9bd80d18c7e65e166dddc9b0ab1
Xinyi Li <xinyili@Xinyis-MacBook-Pro.local>
2023-04-12 00:57:57 -0700

    finalized

--- script.js
+++ script.js
@@ -4,14 +4,18 @@
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
+    const bannerImg = document.querySelector('#bannerImg');
     const sections = document.querySelectorAll('section')
     let mode = 'dark';
+
+    
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
+            document.getElementById('bannerImg').src = 'media/dark.png'
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -20,10 +24,13 @@
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
+            document.getElementById('bannerImg').src = 'media/light.png'
             for (const section of sections) {
                 section.removeAttribute('class');
             }
             mode = 'dark'
         }
     })
+
+
 })()
